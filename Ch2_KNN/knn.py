573c611ee62494eef196d0fa12ffb580808dd8c1
wchstrife <wch960426@126.com>
2019-07-24 20:44:32 +0800

    KNN

--- Ch2_KNN/knn.py
+++ Ch2_KNN/knn.py
@@ -49,15 +49,28 @@ def autoNorm(dataSet):
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
     normDataSet = zeros(shape(dataSet))
-    m = dataSet.shape(0)
+    m = dataSet.shape[0]
     normDataSet = dataSet - tile(minVals, (m, 1))   # 重复为m*3的矩阵
     normDataSet = normDataSet / tile(ranges, (m,1))
 
     return normDataSet, ranges, minVals
 
+# 约会数据测试
+def datingClassTest():
+    hoRatio = 0.10       # 划分训练集和测试集的比例
+    datingDataMat, datingLabels = file2matrix('Ch02/datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)      # 测试集数目
+    errorCount = 0.0
 
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs : m, :], datingLabels[numTestVecs : m], 3) 
+        print("the classifier came back with : %d, the real answer is : %d " % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+
+    print("the total error rate is: %f " % (errorCount / float(numTestVecs)))
 
 if __name__ == "__main__":
-    group, labels = createDataSet()
-    inX = [0, 0]
-    classify0(inX, group, labels, 3)
+    datingClassTest()
